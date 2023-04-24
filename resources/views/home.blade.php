d456bfd59d13ef4004c080da0b593ec6415d17f1
roger medico <rmedico@nrs-group.com>
2023-04-24 18:05:39 +0200

    home tweaks

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,33 +18,24 @@
             <div class="ps-3">
                 <p>
                     {{__('This authentication token allows you to retrieve (GET) and create (POST) DB records as you please, but you can just modify (PUT) or
-                    delete (DELETE) records created with the same authentication token. The column <code>token_id</code> from the DB tables shows the id from the token used
+                    delete (DELETE) records created with the same authentication token. The column')}} <code>token_id</code> {{__('from the DB tables shows the id from the token used
                     to create the record.')}}
                 </p>
                 <p>{{__('The authentication is made with a Bearer token. To correctly authenticate your requests a header entry
                     specifying the authentication of the request should be added. To quickly allow you to identify your records the
-                    id of the key is the first number, so if your key starts with a 12 the records that his <code>token_id</code> is equal
+                    id of the key is the first number, so if your key starts with a 12 the records that his')}} <code>token_id</code> {{__('is equal
                     to 12 are your records, and you are allowed to modify or delete.')}}
                 </p>
-                <p>{{__('An example of the authentication header that has to be included in every request could be as follows
-                    <code>Authorization: Bearer @{{token provided}}</code> and remember that requests to APIs also has to
-                    set the Accept header to application/json (<code>Accept: application/json</code>).')}}</p>
+                <p>{{__('An example of the authentication header that has to be included in every request could be as follows')}}
+                    <code>Authorization: Bearer @{{token provided}}</code> {{__('and remember that requests to APIs also has to
+                    set the Accept header to application/json')}} (<code>Accept: application/json</code>).</p>
 
                 <p>{{__('Tokens will be valid for 24 hours.')}}</p>
                 <div class="text-center">
-                    <div class="d-sm-none table-responsive">
-                        <table class="table table-borderless mt-2">
-                            <tr>
-                                <td>
-                                    <span id="token" class="alert alert-success text-center font-monospace authentication-key">{{$api_token}}</span>
-                                </td>
-                            </tr>
-                        </table>
+                    <span id="token" class="alert alert-success text-center font-monospace authentication-key">{{$api_token}}</span>
+                    <div class="mt-3">
+                    <button id="copy-clipboard" class="btn btn-primary">{{__('Copy to clipboard')}}</button>
                     </div>
-                    <span id="token" class="d-none d-sm-inline alert alert-success text-center font-monospace authentication-key">{{$api_token}}</span>
-                    <a id="copy-clipboard" class="ms-2" title="{{__('Copy to clipboard')}}" alt="{{__('Copy to clipboard')}}">
-                        <x-heroicon-o-clipboard-copy class="icon"/>
-                    </a>
                 </div>
             </div>
         </section>
@@ -52,8 +43,8 @@
             <h1>{{__('api documentation')}}</h1>
             <div class="ps-3">
                 <p>
-                    {{__('Documentation done with <a href="https://swagger.io/" alt="Swagger">Swagger</a> using the composer
-                    packet <a href="https://github.com/DarkaOnLine/L5-Swagger" alt="DarkaOnLine/L5-Swagger">darkaonline/l5-swagger</a>.')}}
+                    {{__('Documentation done with')}} <a href="https://swagger.io/" alt="Swagger">Swagger</a> {{__('using the composer package')}}
+                    <a href="https://github.com/DarkaOnLine/L5-Swagger" alt="DarkaOnLine/L5-Swagger">darkaonline/l5-swagger</a>.
                 </p>
                 <div class="text-center text-md-start">
                     <a class="btn btn-primary" href="{{url('api/documentation')}}">{{__('Open documentation')}}</a>
